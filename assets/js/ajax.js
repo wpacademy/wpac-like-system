1fe536aea2b7748cdc8809eace9dce0a1fe3e63e
Mian Shahzad Raza <info@wpacademy.pk>
2018-10-04 19:01:00 +0500

    Added user logged-in check

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,19 +1,24 @@
 function wpac_like_btn_ajax(postId,usrid) {
     
 	var post_id = postId;
-    var usr_ID = usrid;
-	jQuery.ajax({
-		url : wpac_ajax_url.ajax_url,
-		type : 'post',
-		data : {
-			action : 'wpac_like_btn_ajax_action',
-			pid : post_id,
-			uid : usr_ID
-		},
-		success : function( response ) {
-            jQuery("#wpacAjaxResponse span").html(response);
-		}
-	});
+	var usr_ID = usrid;
+	if(usr_ID === 0) {
+		alert("You must be logged-in to like");
+	} else {
+		jQuery.ajax({
+			url : wpac_ajax_url.ajax_url,
+			type : 'post',
+			data : {
+				action : 'wpac_like_btn_ajax_action',
+				pid : post_id,
+				uid : usr_ID
+			},
+			success : function( response ) {
+				jQuery("#wpacAjaxResponse span").html(response);
+			}
+		});
+	}
+	
 }
 
 function wpac_dislike_btn_ajax(postId,usrid) {
